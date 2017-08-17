28b65f1b3c59b680d14e6ded4ea38c1ba05ae9a9
Corbfon <corbin.fonville@gmail.com>
2017-08-17 14:23:09 -0500

    initial commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'test-comp',
+  templateUrl: './test.component.pug'
+})
+
+export class TestComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
