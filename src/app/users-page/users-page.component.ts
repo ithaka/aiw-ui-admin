baf1be477f84b96ee84314fbbe9c32c946739dd2
Corbfon <corbin.fonville@gmail.com>
2017-09-14 13:22:16 -0500

    make user modal open on list click

--- src/app/users-page/users-page.component.ts
+++ src/app/users-page/users-page.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective } from 'ng2-table/ng2-table';
+import { Component, OnInit } from '@angular/core'
+import { NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective } from 'ng2-table/ng2-table'
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
 
-import { AuthService, UsersService } from './../shared';
+import { AuthService, UsersService } from './../shared'
+import { UserDetailsModal } from './../modals'
 
 @Component({
   selector: 'ang-users-page',
@@ -31,115 +33,116 @@ export class UsersPage implements OnInit {
     sorting: {columns: this.columns},
     filtering: {filterString: ''},
     className: ['table-striped', 'table-bordered']
-  };
+  }
 
   constructor(
     private _auth: AuthService,
-    private _users: UsersService
+    private _users: UsersService,
+    private _modal: NgbModal
   ) {
     
   }
   
   public ngOnInit():void {
-    this.loadUsers();
+    this.loadUsers()
   }
 
   private loadUsers(): void{
     this._users.getUsers().subscribe( (res) => {
       if(res){
-        this.users = res;
-        this.length = this.users.length;
-        this.onChangeTable(this.config);
+        this.users = res
+        this.length = this.users.length
+        this.onChangeTable(this.config)
       }
     })
   }
 
   public changePage(page:any, data:Array<any> = this.users):Array<any> {
-    let start = (page.page - 1) * page.itemsPerPage;
-    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
-    return data.slice(start, end);
+    let start = (page.page - 1) * page.itemsPerPage
+    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length
+    return data.slice(start, end)
   }
 
   public changeSort(data:any, config:any):any {
     if (!config.sorting) {
-      return data;
+      return data
     }
 
-    let columns = this.config.sorting.columns || [];
-    let columnName:string = void 0;
-    let sort:string = void 0;
+    let columns = this.config.sorting.columns || []
+    let columnName:string = void 0
+    let sort:string = void 0
 
     for (let i = 0; i < columns.length; i++) {
       if (columns[i].sort !== '' && columns[i].sort !== false) {
-        columnName = columns[i].name;
-        sort = columns[i].sort;
+        columnName = columns[i].name
+        sort = columns[i].sort
       }
     }
 
     if (!columnName) {
-      return data;
+      return data
     }
 
     // simple sorting
     return data.sort((previous:any, current:any) => {
       if (previous[columnName] > current[columnName]) {
-        return sort === 'desc' ? -1 : 1;
+        return sort === 'desc' ? -1 : 1
       } else if (previous[columnName] < current[columnName]) {
-        return sort === 'asc' ? -1 : 1;
+        return sort === 'asc' ? -1 : 1
       }
-      return 0;
-    });
+      return 0
+    })
   }
 
   public changeFilter(data:any, config:any):any {
-    let filteredData:Array<any> = data;
+    let filteredData:Array<any> = data
     this.columns.forEach((column:any) => {
       if (column.filtering) {
         filteredData = filteredData.filter((item:any) => {
-          return item[column.name].match(column.filtering.filterString);
-        });
+          return item[column.name].match(column.filtering.filterString)
+        })
       }
-    });
+    })
 
     if (!config.filtering) {
-      return filteredData;
+      return filteredData
     }
 
     if (config.filtering.columnName) {
       return filteredData.filter((item:any) =>
-        item[config.filtering.columnName].match(this.config.filtering.filterString));
+        item[config.filtering.columnName].match(this.config.filtering.filterString))
     }
 
-    let tempArray:Array<any> = [];
+    let tempArray:Array<any> = []
     filteredData.forEach((item:any) => {
-      let flag = false;
+      let flag = false
       this.columns.forEach((column:any) => {
         if (item[column.name].toString().match(this.config.filtering.filterString)) {
-          flag = true;
+          flag = true
         }
-      });
+      })
       if (flag) {
-        tempArray.push(item);
+        tempArray.push(item)
       }
-    });
-    filteredData = tempArray;
+    })
+    filteredData = tempArray
 
-    return filteredData;
+    return filteredData
   }
 
   public onChangeTable(config:any, page:any = {page: this.page, itemsPerPage: this.itemsPerPage}):any {
     if (config.filtering) {
-      Object.assign(this.config.filtering, config.filtering);
+      Object.assign(this.config.filtering, config.filtering)
     }
 
     if (config.sorting) {
-      Object.assign(this.config.sorting, config.sorting);
+      Object.assign(this.config.sorting, config.sorting)
     }
 
-    // let filteredData = this.changeFilter(this.users, this.config);
-    let sortedData = this.changeSort(this.users, this.config);
-    this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
-    this.length = sortedData.length;
+    // let filteredData = this.changeFilter(this.users, this.config)
+    let sortedData = this.changeSort(this.users, this.config)
+    this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData
+    this.length = sortedData.length
   }
 
   public onCellClick(data: any): any {
