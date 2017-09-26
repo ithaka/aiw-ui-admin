d9b4575e74098b694de41ad9ff7b03da0b74d653
Corbfon <corbin.fonville@gmail.com>
2017-09-26 15:49:03 -0500

    modify getUsers call url method

--- src/app/shared/users.service.ts
+++ src/app/shared/users.service.ts
@@ -42,8 +42,7 @@ export class UsersService {
    */
   public getUsers(): Observable<ListUsersResponse[]> {
     return this.http.get<ListUsersResponse[]>(
-      'http://art-aa-service.apps.test.cirrostratus.org/admin/users/manageUsers?type=active',
-      // [this.getServiceUrl(true), "users", "manageUsers"].join("/") + '?type=active',
+      [this._auth.getServiceUrl(), "users", "manageUsers"].join("/") + '?type=active',
       { withCredentials: true }
     )
   }
