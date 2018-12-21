0e33e31fd63e3bb23f354f6f384f24446df4f2ab
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2018-12-21 15:45:45 +0500

    Users List: Look for Forum Access in roles field instead of ssEnabled!

--- src/app/shared/users.service.ts
+++ src/app/shared/users.service.ts
@@ -64,7 +64,8 @@ export class UsersService {
       // Format values for User Status & SSenabled to show in grid cells
       for(let user of resultArray){ 
         user.status = user.active ? 'Active' : 'Archive'
-        user.ssValue = user.ssenabled ? '<img src="/assets/img/checkMark.gif" class="tickIcon">' : ''
+        // user.ssValue = user.ssenabled ? '<img src="/assets/img/checkMark.gif" class="tickIcon">' : ''
+        user.ssValue = user.roles.indexOf("SHARED_SHELF_USER") > -1 ? '<img src="/assets/img/checkMark.gif" class="tickIcon">' : ''
         
         // Making sure that the date format is compatible with the date pipe on all browsers incl. Safari
         user.timelastaccessed = this.formatDate(user.timelastaccessed)
